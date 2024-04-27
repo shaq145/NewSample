33d8c3a876739053e8632482cd7dcb874d1bb5fe
Norvin Shaq Lintag <nslintag06@gmail.com>
2024-04-27 22:51:14 +0800

    04/27/2024 Updates: Edited PlayerController scripts, Edited Test Scene, Added Player Prefabs

--- Assets/Scripts/ShaqScripts/PlayerController.cs
+++ Assets/Scripts/ShaqScripts/PlayerController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody2D rgbd2d;
+    private Vector2 movements;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rgbd2d = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        movements.x = Input.GetAxisRaw ( "Horizontal" );
+        rgbd2d.velocity = new Vector2 ( movements.x * moveSpeed, rgbd2d.velocity.y );
     }
+
 }
